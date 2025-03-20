6bad1af63ee3f6e3e7629cef85319996a7faaf0e
qexdy3 <154340027+qexdy3@users.noreply.github.com>
2025-03-20 14:49:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
-
+from aiogram.fsm.storage.memory import MemoryStorage  # Простое хранилище состояний
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=MemoryStorage())  # Добавляем хранилище
