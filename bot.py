a2be24f82abbf6758accaaac140e2168ee842ad1
qexdy3 <154340027+qexdy3@users.noreply.github.com>
2025-03-20 15:08:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,3 @@ from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
-async def on_startup():
-    await bot.set_webhook(os.getenv("WEBHOOK_URL"))
-
-dp.startup.register(on_startup)
