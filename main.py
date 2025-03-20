95e99464f4f22d8e42d9343ce8aaa8f2bf9ec0b2
qexdy3 <154340027+qexdy3@users.noreply.github.com>
2025-03-20 16:38:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import logging
 import asyncio
+from flask import Flask
 from bot import dp, bot
 import handlers  # noqa: F401 - чтобы загрузились обработчики
-from flask import Flask
 
-# Настраиваем логирование
 logging.basicConfig(level=logging.INFO)
 
-# Создаем Flask-сервер
+# Создаём Flask-сервер
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,8 +17,7 @@ async def start_bot():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.create_task(start_bot())  # Запуск бота в асинхронной задаче
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())  # Запуск бота как асинхронную задачу
     app.run(host="0.0.0.0", port=8000)
     
