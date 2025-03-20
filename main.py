40459f9524ae4f100e9ee6f049319ec244f1fe3b
qexdy3 <154340027+qexdy3@users.noreply.github.com>
2025-03-20 15:29:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import logging
-from bot import dp, bot
+import asyncio
+from bot import dp, bot, web_server
 import handlers  # noqa: F401 - чтобы загрузились обработчики
+from aiohttp import web
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    
+    # Запускаем веб-сервер
+    app = await web_server()
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)  # Сервер будет слушать все IP на порту 8080
+    await site.start()
+    
+    # Запускаем бота
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    dp.run_polling(bot)
+    asyncio.run(main())
